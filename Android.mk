SlimRoms/hardware_qcom_keymaster

keymaster: add msm8996 into support list
 * we have the flag in op3 boardconfig but it's not here
 * probably the multirom issue fixed too

Change-Id: Ic3d6c4da78f1e75f73d4047cd810d834607d9bd0
Signed-off-by: Martinusbe <martinusbe@gmail.com>

Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-ifneq ($(filter msm8909 msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994 msm8998,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8909 msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)

NinjaOS/hardware_key_master

Android.mk
@@ -3,7 +3,7 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8998,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)